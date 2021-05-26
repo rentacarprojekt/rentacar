bdb4f0a5534d86acaff694feeccf524ce6a65afb
rentacarprojekt <rent.a.car.admn@gmail.com>
2021-05-26 18:37:34 +0200

    Dorađena registracija korisnika i kreirane iznimke

--- rent-a-car/src/main/java/hr/tvz/project/service/UserService.java
+++ rent-a-car/src/main/java/hr/tvz/project/service/UserService.java
@@ -2,11 +2,14 @@ package hr.tvz.project.service;
 
 import org.springframework.stereotype.Service;
 
+import hr.tvz.project.dto.UserDetailsDto;
 import hr.tvz.project.dto.UserRegistrationDto;
+import hr.tvz.project.exceptions.UsernameOrEmailAlreadyInUseException;
+import hr.tvz.project.model.User;
 
 @Service
 public interface UserService {
 	
-	public void createNewUser(UserRegistrationDto newUser);
+	public UserDetailsDto createNewUser(UserRegistrationDto newUser) throws UsernameOrEmailAlreadyInUseException;
 
 }
